b106fb051d1eef31ddd7ba1e8371940dab463542
AugustoGalarza99 <augustogalarza15@gmail.com>
2023-09-11 18:26:35 -0300

    Primera subida de proyecto

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//Función para generar un número aleatorio entre 1 y 100
+function generarNumeroAleatorio() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+// Función principal del juego
+    function jugarAdivinanza() {
+    const numeroAleatorio = generarNumeroAleatorio();
+    let intentos = 0;
+    let numeroAdivinado = false;
+
+    alert("Adivina el número secreto entre 1 y 100.");
+
+    while (!numeroAdivinado) {
+        const intento = parseInt(prompt("Ingresa tu adivinanza:"));
+            if (isNaN(intento)) {
+            alert("Por favor, ingresa un número válido.");
+        } else {
+        intentos++;
+
+        if (intento < numeroAleatorio) {
+          alert("El número es mayor.");
+        } else if (intento > numeroAleatorio) {
+          alert("El número es menor.");
+        } else {
+          alert(`¡Correcto! Adivinaste el número ${numeroAleatorio} en ${intentos} intentos.`);
+          numeroAdivinado = true;
+        }
+      }
+    }
+  }
+  
+//Iniciar el juego
+    jugarAdivinanza();
